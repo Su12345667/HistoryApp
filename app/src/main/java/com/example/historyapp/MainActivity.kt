105f7b23466f8fda85b8f40886cd65b35f2b30e9
Su12345667 <ST10442427@vcconnect.edu.za>
2024-04-04 16:05:38 +0200

    Third

--- app/src/main/java/com/example/historyapp/MainActivity.kt
+++ app/src/main/java/com/example/historyapp/MainActivity.kt
@@ -23,15 +23,48 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+
         val exitBtn = findViewById<Button>(R.id.exitBtn)
         val resetBtn = findViewById<Button>(R.id.resetBtn)
         val generateBtn = findViewById<Button>(R.id.generateBtn)
+        val txt = findViewById<TextView>(R.id.display)
+        val numTxt = findViewById<EditText>(R.id.editTextNumber)
+        val ages = listOf(22, 32, 34, 78, 63, 41, 55, 82, 98, 84,95,56)
+        val names = listOf("Black Dahlia", "Brittany Murphy", "Jeffrey Dahmer",
+    "Andrew Jackson", "Donna Summer", "Kobe Bryant", "Emily Dickinson", "Neil Armstrong",
+            "Pierre Cardin", "Sir Isaac Newton", "Nelson Mandela","Adolf Hitler")
 
-        exitBtn.setOnClickListener {
-            exitProcess(1)
-        }
-        generateBtn.setOnClickListener {
-        }
+resetBtn.setOnClickListener {
+    txt.text = ""
+    numTxt.setText("")
+}
 
+exitBtn.setOnClickListener {
+    finish() // Close the activity
+}
+
+generateBtn.setOnClickListener {
+    val age = numTxt.text.toString()
+    if (age.isNotEmpty()) {
+        val age = age.toInt()
+        if (age in 20..100) {
+            if (ages.contains(age)) {
+                val nameOf = ages.indexOf(age)
+                txt.text = "You match with ${names[nameOf]}"
+            } else {
+                txt.text = "Your age doesn't match"
+            }
+        } else {
+            txt.text = "Enter a valid number between 20 to 100"
+        }
+    } else {
+        txt.text = "Please enter your age"
     }
 }
+
+
+}
+
+}
+
