1815d0c20627e07bc7107c57d64cbf6a4a0b7bf2
Su12345667 <ST10442427@vcconnect.edu.za>
2024-04-04 07:37:25 +0200

    Second

--- app/src/main/java/com/example/historyapp/MainActivity.kt
+++ app/src/main/java/com/example/historyapp/MainActivity.kt
@@ -10,4 +10,28 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 }
-val = exitProcess(Int)
+package com.example.thehistoryapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import kotlin.system.exitProcess
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val exitBtn = findViewById<Button>(R.id.exitBtn)
+        val resetBtn = findViewById<Button>(R.id.resetBtn)
+        val generateBtn = findViewById<Button>(R.id.generateBtn)
+
+        exitBtn.setOnClickListener {
+            exitProcess(1)
+        }
+        generateBtn.setOnClickListener {
+        }
+
+    }
+}
